84edd774202e5bb5bc30d55d157516e2e97f1aef
oddjobsman <oddjobsman@ranganathkini.com>
2013-11-15 13:14:18 +0530

    corrected indentation on files

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,55 +5,56 @@
  */
 
 module.exports = function (grunt) {
-    'use strict';
+  'use strict';
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        complexity: {
-            generic: {
-                src: ['lib/**/*.js'],
-                options: {
-                    errorsOnly: false,
-                    cyclometric: 6,       // default is 3
-                    halstead: 16,         // default is 8
-                    maintainability: 100  // default is 100
-                }
-            }
-        },
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'lib/**/*.js',
-                'test/**/*.js'
-            ],
-            options: {
-                jshintrc: '.jshintrc'
-            }
-        },
-        mochacli: {
-            all: ['test/**/*.js'],
-            options: {
-                reporter: 'spec',
-                ui: 'bdd'
-            }
-        },
-        watch: {
-            js: {
-                files: ['**/*.js', '!node_modules/**/*.js'],
-                tasks: ['default'],
-                options: {
-                    nospawn: true
-                }
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    complexity: {
+      generic: {
+        src: ['lib/**/*.js'],
+        options: {
+          errorsOnly: false,
+          cyclometric: 6,       // default is 3
+          halstead: 16,         // default is 8
+          maintainability: 100  // default is 100
         }
-    });
+      }
+    },
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'lib/**/*.js',
+        'test/**/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    mochacli: {
+      all: ['test/**/*.js'],
+      options: {
+        reporter: 'spec',
+        ui: 'bdd',
+        require: ['expect.js']
+      }
+    },
+    watch: {
+      js: {
+        files: ['**/*.js', '!node_modules/**/*.js'],
+        tasks: ['default'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-complexity');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-complexity');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-cli');
 
-    grunt.registerTask('test', ['jshint', 'mochacli', 'complexity', 'watch']);
-    grunt.registerTask('ci', ['jshint', 'mochacli', 'complexity']);
-    grunt.registerTask('default', ['test']);
+  grunt.registerTask('test', ['jshint', 'mochacli', 'complexity', 'watch']);
+  grunt.registerTask('ci', ['jshint', 'mochacli', 'complexity']);
+  grunt.registerTask('default', ['test']);
 };
