465d24d17014e50b77660eab383151e1b147a078
oddjobsman <oddjobsman@ranganathkini.com>
2013-11-15 21:46:11 +0530

    Renamed circuit box tests and minor refactoring to circuit box

---
+++ test/circuitbox.test.js
@@ -0,0 +1,54 @@
+/*!
+ * circuitbox
+ * Copyright(c) 2013 Ranganath Kini <oddjobsman@ranganathkini.com>
+ * MIT Licensed
+ */
+
+(function () {
+  'use strict';
+
+  var context = describe;
+  var expect = require('expect.js');
+
+  var CircuitBox = require('../lib/circuitbox');
+  var NoSuchComponentDefinitionException = require('../lib/components/nosuchcomponentdefinitionexception');
+
+  describe('CircuitBox', function () {
+
+    context('when created', function () {
+      var cbx = new CircuitBox();
+
+      it('should be empty', function () {
+        expect(cbx.hasComponents()).to.be(false);
+      });
+
+      it('should throw error if an unregistered component is requested', function () {
+        expect(function () {
+          cbx.get('unregisteredComponent');
+        }).to.throwException(function (e) {
+            expect(e).to.be.a(NoSuchComponentDefinitionException);
+            expect(e.message).to.match(/Component 'unregisteredComponent' could not be found/);
+          });
+      });
+
+    });
+
+    context('when created without a name', function () {
+      var registry = new CircuitBox();
+
+      it('should not have a name', function () {
+        expect(registry.name()).not.to.be.ok();
+      });
+
+    });
+
+    context('when created with a name', function () {
+      var registry =  new CircuitBox({name: 'test'});
+
+      it('should have the specified name', function () {
+        expect(registry.name()).to.be('test');
+      });
+
+    });
+  });
+})();
