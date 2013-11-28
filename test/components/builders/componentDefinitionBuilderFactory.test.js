1fce04a2dfe6d32eb54a83856583b710e6929749
oddjobsman <oddjobsman@ranganathkini.com>
2013-11-29 00:59:19 +0530

    added ComponentDefinitionBuilderFactory

---
+++ test/components/builders/componentDefinitionBuilderFactory.test.js
@@ -0,0 +1,53 @@
+/*!
+ * circuitbox
+ * Copyright(c) 2013 Ranganath Kini <oddjobsman@ranganathkini.com>
+ * MIT Licensed
+ */
+
+(function () {
+  'use strict';
+
+  var context = describe;
+  var expect = require('expect.js');
+
+  var SimpleComponentDefinitionBuilder = require('../../../lib/components/builders/simpleComponentDefinitionBuilder');
+  var ModuleBasedComponentDefinitionBuilder = require('../../../lib/components/builders/moduleBasedComponentDefinitionBuilder');
+
+  var ComponentDefinitionBuilderFactory = require('../../../lib/components/builders');
+
+  describe('ComponentDefinitionBuilderFactory', function () {
+
+    context('when created with a component name', function () {
+      var factory;
+
+      beforeEach(function () {
+        factory = new ComponentDefinitionBuilderFactory('myComponent');
+      });
+
+      it('should return a SimpleComponentDefinitionBuilder when #use() is invoked with an object', function () {
+        var objectValue = 'This is a value';
+
+        var definition = factory.use(objectValue);
+
+        expect(definition).to.be.a(SimpleComponentDefinitionBuilder);
+        expect(definition.options.name).to.be('myComponent');
+        expect(definition.options.object).to.be(objectValue);
+
+      });
+
+      it('should return a ModuleBasedComponentDefinitionBuilder when #require() is invoked with an module-id', function () {
+        var moduleId = './myComponentModule';
+
+        var definition = factory.require(moduleId);
+
+        expect(definition).to.be.a(ModuleBasedComponentDefinitionBuilder);
+        expect(definition.options.name).to.be('myComponent');
+        expect(definition.options.moduleId).to.be(moduleId);
+
+      });
+
+    });
+
+  });
+
+})();
