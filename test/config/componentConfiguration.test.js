cab536149fc989cf1d0beed643fcf6bd226803c8
oddjobsman <oddjobsman@ranganathkini.com>
2013-12-02 11:53:50 +0530

    renaming file back

--- test/config/componentConfiguration.test.js
+++ test/config/componentConfiguration.test.js
@@ -10,7 +10,7 @@
   var expect = require('expect.js');
 
   var definitions = require('../../lib/definitions');
-  var ComponentConfiguration = require('../../lib/config/configuration');
+  var ComponentConfiguration = require('../../lib/config/componentConfiguration');
   var ConfigurationDefinitionBuilderFactory = require('../../lib/definitions/builders');
 
   describe('ComponentConfiguration', function () {
