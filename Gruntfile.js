c847e7e6e44d42d7ae57bc5ffb796577dbc46855
oddjobsman <oddjobsman@ranganathkini.com>
2013-12-15 17:08:55 +0530

    modified Gruntfile.js to be strict

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,65 +4,71 @@
  * MIT Licensed
  */
 
-module.exports = function (grunt) {
+(function () {
   'use strict';
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    complexity: {
-      generic: {
-        src: ['lib/**/*.js'],
+  module.exports = function (grunt) {
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      complexity: {
+        generic: {
+          src: ['lib/**/*.js'],
+          options: {
+            errorsOnly: false,
+            cyclometric: 6,       // default is 3
+            halstead: 16,         // default is 8
+            maintainability: 100  // default is 100
+          }
+        }
+      },
+      jshint: {
+        all: [
+          'Gruntfile.js',
+          'lib/**/*.js',
+          'test/**/*.js'
+        ],
         options: {
-          errorsOnly: false,
-          cyclometric: 6,       // default is 3
-          halstead: 16,         // default is 8
-          maintainability: 100  // default is 100
+          jshintrc: '.jshintrc'
         }
-      }
-    },
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'lib/**/*.js',
-        'test/**/*.js'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
-    mochacli: {
-      all: ['test/**/*.js'],
-      options: {
-        reporter: 'spec',
-        ui: 'bdd',
-        require: ['expect.js']
-      }
-    },
-    sloc: {
-      scripts: {
-        files: {
-          'lib': ['**.js']
-        }
-      }
-    },
-    watch: {
-      js: {
-        files: ['**/*.js', '!node_modules/**/*.js'],
-        tasks: ['default'],
+      },
+      mochacli: {
+        all: ['test/**/*.js'],
         options: {
-          nospawn: true
+          reporter: 'spec',
+          ui: 'bdd',
+          require: ['expect.js']
+        }
+      },
+      sloc: {
+        scripts: {
+          files: {
+            'lib': ['**.js']
+          }
+        }
+      },
+      watch: {
+        js: {
+          files: ['**/*.js', '!node_modules/**/*.js'],
+          tasks: ['default'],
+          options: {
+            nospawn: true
+          }
         }
       }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-complexity');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.loadNpmTasks('grunt-sloc');
+    grunt.loadNpmTasks('grunt-complexity');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-mocha-cli');
+    grunt.loadNpmTasks('grunt-sloc');
 
-  grunt.registerTask('test', ['jshint', 'mochacli', 'complexity', 'sloc', 'watch']);
-  grunt.registerTask('ci', ['jshint', 'mochacli', 'complexity', 'sloc']);
-  grunt.registerTask('default', ['test']);
-};
+    grunt.registerTask('test', ['jshint', 'mochacli', 'complexity', 'sloc', 'watch']);
+    grunt.registerTask('ci', ['jshint', 'mochacli', 'complexity', 'sloc']);
+    grunt.registerTask('default', ['test']);
+  };
+
+})();
+
+
+
