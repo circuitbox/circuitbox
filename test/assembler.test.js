37143d9a5cd6354e418a8c70b254567cb8220308
oddjobsman <oddjobsman@ranganathkini.com>
2013-12-30 00:03:08 +0530

    dependency-injection WORKS! WOOOOHOOO!

--- test/assembler.test.js
+++ test/assembler.test.js
@@ -7,6 +7,7 @@
 'use strict';
 
 var expect = require('expect.js');
+var utils = require('../lib/utils');
 var sinon = require('sinon');
 
 var Scopes = require('../lib/scopes');
@@ -17,6 +18,20 @@ var AssemblyContext = require('../lib/assemblyContext');
 var Assembler = require('../lib/assembler');
 
 describe('Assembler', function () {
+  var registryApi,
+      mockRegistry;
+
+  beforeEach(function () {
+    registryApi = {
+      assemblyListFor: function () {}
+    };
+
+    mockRegistry = sinon.mock(registryApi);
+  });
+
+  afterEach(function () {
+    mockRegistry.verify();
+  });
 
   it('should create a new Assembler for the specified AssemblyContext', function () {
     var assembler = Assembler.for(new AssemblyContext('myComponent', {}));
@@ -27,20 +42,14 @@ describe('Assembler', function () {
     expect(function () {
       Assembler.for({});
     }).to.throwError(function (e) {
-          expect(e).to.be.a(ComponentCreationError);
-          expect(e.message).to.match(/Cannot assemble component without a valid assembly context/);
-        });
+      expect(e).to.be.a(ComponentCreationError);
+      expect(e.message).to.match(/Cannot assemble component without a valid assembly context/);
+    });
   });
 
-  it('should assemble target component and return to specified handler', function (done) {
-    var registryApi = {
-      assemblyListFor: function () {}
-    };
-
+  it('should assemble target component and return to specified callback', function (done) {
     var targetComponentName = 'myComponent';
     var componentValue = 'This is my message';
-
-    var mockRegistry = sinon.mock(registryApi);
 
     mockRegistry.expects('assemblyListFor').withArgs('myComponent').returns([
       new SimpleComponentDefinition({
@@ -59,15 +68,149 @@ describe('Assembler', function () {
     });
   });
 
-  it('should assemble target component and return a promise that will return the component value to completion handler', function (done) {
-    var registryApi = {
-      assemblyListFor: function () {}
-    };
+  it('should assemble target component with dependencies and return to specified callback', function (done) {
+    var targetComponentName = 'message';
+    var location = 'Bangalore';
+    var message = 'This is my %s';
 
+    var locationComponentDefinition = new SimpleComponentDefinition({
+      name: 'location',
+      scope: Scopes.singleton,
+      component: location
+    });
+
+    var messageComponentDefinition = new SimpleComponentDefinition({
+      name: targetComponentName,
+      scope: Scopes.singleton,
+      dependencies: ['location'],
+      component: function (deps) {
+        return utils.sprintf(message, deps.location);
+      }
+    });
+
+    mockRegistry.expects('assemblyListFor').withArgs('message').returns([
+      locationComponentDefinition,
+      messageComponentDefinition
+    ]).once();
+
+    mockRegistry.expects('assemblyListFor').withArgs('location').returns([
+      locationComponentDefinition
+    ]).once();
+
+    Assembler.for(new AssemblyContext('message', {
+      registry: registryApi
+    })).assemble(function (err, value) {
+      if (err) {
+        console.log(err);
+        done();
+        return;
+      }
+      expect(value).to.be('This is my Bangalore');
+      done();
+    });
+  });
+
+  it('should assemble target component with multi-level dependencies and return to specified callback', function (done) {
+    var targetComponentName = 'printer';
+
+    var citiesComponentDefinition = new SimpleComponentDefinition({
+      name: 'cities',
+      scope: Scopes.singleton,
+      component: ['Mumbai', 'Bangalore']
+    });
+
+    var locationComponentDefinition = new SimpleComponentDefinition({
+      name: 'location',
+      scope: Scopes.singleton,
+      dependencies: ['cities'],
+      component: function (deps) {
+        return deps.cities[0];
+      }
+    });
+
+    var utilsComponentDefinition = new SimpleComponentDefinition({
+      name: 'utils',
+      scope: Scopes.singleton,
+      component: utils
+    });
+
+    var languagesComponentDefinition = new SimpleComponentDefinition({
+      name: 'languages',
+      scope: Scopes.singleton,
+      component: {
+        'Mumbai': 'Marathi',
+        'Bangalore': 'Kannada'
+      }
+    });
+
+    var messageComponentDefinition = new SimpleComponentDefinition({
+      name: 'message',
+      scope: Scopes.singleton,
+      dependencies: ['languages', 'utils', 'location'],
+      component: function (deps) {
+        var location = deps.location;
+        var language = deps.languages[location];
+        return deps.utils.sprintf('This is my %s. We speak %s here.', location, language);
+      }
+    });
+
+    var printerComponentDefinition = new SimpleComponentDefinition({
+      name: targetComponentName,
+      scope: Scopes.singleton,
+      dependencies: ['message', 'utils', 'cities'],
+      component: function (deps) {
+        return deps.utils.sprintf('%s You may also want to travel to %s', deps.message, deps.cities[1]);
+      }
+    });
+
+    mockRegistry.expects('assemblyListFor').withArgs(targetComponentName).returns([
+      messageComponentDefinition,
+      utilsComponentDefinition,
+      citiesComponentDefinition,
+      printerComponentDefinition
+    ]).once();
+
+    mockRegistry.expects('assemblyListFor').withArgs('message').returns([
+      utilsComponentDefinition,
+      languagesComponentDefinition,
+      locationComponentDefinition,
+      messageComponentDefinition
+    ]).once();
+
+    mockRegistry.expects('assemblyListFor').withArgs('location').returns([
+      citiesComponentDefinition,
+      locationComponentDefinition
+    ]).once();
+
+    mockRegistry.expects('assemblyListFor').withArgs('utils').returns([
+      utilsComponentDefinition
+    ]).exactly(2);
+
+    mockRegistry.expects('assemblyListFor').withArgs('cities').returns([
+      citiesComponentDefinition
+    ]).exactly(2);
+
+    mockRegistry.expects('assemblyListFor').withArgs('languages').returns([
+      languagesComponentDefinition
+    ]).once();
+
+    Assembler.for(new AssemblyContext(targetComponentName, {
+      registry: registryApi
+    })).assemble(function (err, value) {
+      if (err) {
+        console.log(err);
+        done();
+        return;
+      }
+      expect(value).to.be('This is my Mumbai. We speak Marathi here. You may also want to travel to Bangalore');
+      done();
+    });
+
+  });
+
+  it('should assemble target component and return a promise that will return the component value to completion handler', function (done) {
     var targetComponentName = 'myComponent';
     var componentValue = 'This is my message';
-
-    var mockRegistry = sinon.mock(registryApi);
 
     mockRegistry.expects('assemblyListFor').withArgs('myComponent').returns([
       new SimpleComponentDefinition({
@@ -80,12 +223,10 @@ describe('Assembler', function () {
     Assembler.for(new AssemblyContext('myComponent', {
       registry: registryApi
     })).assemble()
-      .done(function (value) {
-        expect(value).to.be(componentValue);
-        done();
-      }, function (err) {
-          console.log(err);
-        });
+    .done(function (value) {
+      expect(value).to.be(componentValue);
+      done();
+    });
   });
 
 });
