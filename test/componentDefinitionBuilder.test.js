e64ba25cf978e80a36863f0868cf402e69093dac
oddjobsman <oddjobsman@ranganathkini.com>
2013-12-30 17:59:22 +0530

    modified ComponentDefinitionBuilder #dependsOn() method to accept list of dependencies as array, comma-separated string or list of string passed in as arguments

--- test/componentDefinitionBuilder.test.js
+++ test/componentDefinitionBuilder.test.js
@@ -35,6 +35,42 @@ describe('ComponentDefinitionBuilder', function () {
 
     });
 
+    it('should create a ComponentDefinition with the specified name, initializer and dependencies specified as a CSV string', function () {
+      var initializer = function () {};
+      var dependencies = ['a', 'b'];
+
+      var definition = new ComponentDefinitionBuilder('myComponent')
+          .initializeWith(initializer)
+          .dependsOn('a,b')
+          .scope(Scopes.singleton)
+          .build();
+
+      expect(definition).to.be.a(ComponentDefinition);
+      expect(definition.name).to.be('myComponent');
+      expect(definition.scope).to.be(Scopes.singleton);
+      expect(definition.initializer).to.be(initializer);
+      expect(definition.dependencies).to.be.eql(dependencies);
+
+    });
+
+    it('should create a ComponentDefinition with the specified name, initializer and dependencies specified in arguments', function () {
+      var initializer = function () {};
+      var dependencies = ['a', 'b'];
+
+      var definition = new ComponentDefinitionBuilder('myComponent')
+          .initializeWith(initializer)
+          .dependsOn('a', 'b')
+          .scope(Scopes.singleton)
+          .build();
+
+      expect(definition).to.be.a(ComponentDefinition);
+      expect(definition.name).to.be('myComponent');
+      expect(definition.scope).to.be(Scopes.singleton);
+      expect(definition.initializer).to.be(initializer);
+      expect(definition.dependencies).to.be.eql(dependencies);
+
+    });
+
   });
 
 });
