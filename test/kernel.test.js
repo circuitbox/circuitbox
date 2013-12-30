82d0328be09de1de652aedcfee1e60e90b9ca15b
oddjobsman <oddjobsman@ranganathkini.com>
2013-12-30 17:59:52 +0530

    Made Kernel return itself to either callbacks or promise handlers

--- test/kernel.test.js
+++ test/kernel.test.js
@@ -4,6 +4,8 @@
  * MIT Licensed
  */
 
+ /*jshint newcap:false */
+
 'use strict';
 
 var context = describe;
@@ -14,7 +16,7 @@ var Kernel = require('../lib/kernel');
 describe('Kernel', function () {
 
   context('when created with modules', function () {
-    var kernel = new Kernel({
+    Kernel({
       modules: [
         function (registry) {
           registry.for('name').use('John Doe');
@@ -27,47 +29,46 @@ describe('Kernel', function () {
           });
         }
       ]
-    });
-
-    it('should be empty', function () {
-      expect(kernel.hasComponents).to.be(true);
-    });
-
-    it('should return the component to the specified callback', function (done) {
-      kernel.get('message', function (err, message) {
-        expect(err).to.be(null);
-        expect(message).to.be('hello world! this is john doe');
-        done();
+    }, function (err, kernel) {
+      it('should be empty', function () {
+        expect(kernel.hasComponents).to.be(true);
       });
-    });
 
-    it('should return the promise to provide the component to the specified handler', function (done) {
-      kernel.get('message').done(function (message) {
-        expect(message).to.be('hello world! this is john doe');
-        done();
-      }, function (err) {
-        console.log(err);
-        expect().fail();
+      it('should return the component to the specified callback', function (done) {
+        kernel.get('message', function (err, message) {
+          expect(err).to.be(null);
+          expect(message).to.be('hello world! this is john doe');
+          done();
+        });
+      });
+
+      it('should return the promise to provide the component to the specified handler', function (done) {
+        kernel.get('message').done(function (message) {
+          expect(message).to.be('hello world! this is john doe');
+          done();
+        }, function (err) {
+          console.log(err);
+          expect().fail();
+        });
       });
     });
 
   });
 
   context('when created without a name', function () {
-    var kernel = new Kernel();
-
-    it('should not have a name', function () {
-      expect(kernel.name).not.to.be.ok();
-    });
-
+    Kernel()
+      .done(function (kernel) {
+        it('should not have a name', function () {
+          expect(kernel.name).not.to.be.ok();
+        });
+      });
   });
 
   context('when created with a name', function () {
-    var kernel =  new Kernel({name: 'test'});
-
-    it('should have the specified name', function () {
-      expect(kernel.name).to.be('test');
+    Kernel({name: 'test'}, function (err, kernel) {
+      it('should have the specified name', function () {
+        expect(kernel.name).to.be('test');
+      });
     });
-
   });
 });
