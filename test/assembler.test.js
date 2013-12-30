b44d6b0a488c3a0f13e0a13a4b23b023f9ab719a
oddjobsman <oddjobsman@ranganathkini.com>
2013-12-30 09:45:16 +0530

    added tests for promise based API in Assembler

--- test/assembler.test.js
+++ test/assembler.test.js
@@ -100,11 +100,7 @@ describe('Assembler', function () {
     Assembler.for(new AssemblyContext('message', {
       registry: registryApi
     })).assemble(function (err, value) {
-      if (err) {
-        console.log(err);
-        done();
-        return;
-      }
+      expect(err).to.be(null);
       expect(value).to.be('This is my Bangalore');
       done();
     });
@@ -197,11 +193,7 @@ describe('Assembler', function () {
     Assembler.for(new AssemblyContext(targetComponentName, {
       registry: registryApi
     })).assemble(function (err, value) {
-      if (err) {
-        console.log(err);
-        done();
-        return;
-      }
+      expect(err).to.be(null);
       expect(value).to.be('This is my Mumbai. We speak Marathi here. You may also want to travel to Bangalore');
       done();
     });
@@ -227,6 +219,136 @@ describe('Assembler', function () {
       expect(value).to.be(componentValue);
       done();
     });
+  });
+
+  it('should assemble target component with dependencies and return a promise that will return the component value to completion handler', function (done) {
+    var targetComponentName = 'message';
+    var location = 'Bangalore';
+    var message = 'This is my %s';
+
+    var locationComponentDefinition = new SimpleComponentDefinition({
+      name: 'location',
+      scope: Scopes.singleton,
+      component: location
+    });
+
+    var messageComponentDefinition = new SimpleComponentDefinition({
+      name: targetComponentName,
+      scope: Scopes.singleton,
+      dependencies: ['location'],
+      component: function (deps) {
+        return utils.sprintf(message, deps.location);
+      }
+    });
+
+    mockRegistry.expects('assemblyListFor').withArgs('message').returns([
+      locationComponentDefinition,
+      messageComponentDefinition
+    ]).once();
+
+    mockRegistry.expects('assemblyListFor').withArgs('location').returns([
+      locationComponentDefinition
+    ]).once();
+
+    Assembler.for(new AssemblyContext('message', {
+      registry: registryApi
+    })).assemble().done(function (value) {
+      expect(value).to.be('This is my Bangalore');
+      done();
+    });
+  });
+
+  it('should assemble target component with multi-level dependencies  and return a promise that will return the component value to completion handler', function (done) {
+    var targetComponentName = 'printer';
+
+    var citiesComponentDefinition = new SimpleComponentDefinition({
+      name: 'cities',
+      scope: Scopes.singleton,
+      component: ['Mumbai', 'Bangalore']
+    });
+
+    var locationComponentDefinition = new SimpleComponentDefinition({
+      name: 'location',
+      scope: Scopes.singleton,
+      dependencies: ['cities'],
+      component: function (deps) {
+        return deps.cities[0];
+      }
+    });
+
+    var utilsComponentDefinition = new SimpleComponentDefinition({
+      name: 'utils',
+      scope: Scopes.singleton,
+      component: utils
+    });
+
+    var languagesComponentDefinition = new SimpleComponentDefinition({
+      name: 'languages',
+      scope: Scopes.singleton,
+      component: {
+        'Mumbai': 'Marathi',
+        'Bangalore': 'Kannada'
+      }
+    });
+
+    var messageComponentDefinition = new SimpleComponentDefinition({
+      name: 'message',
+      scope: Scopes.singleton,
+      dependencies: ['languages', 'utils', 'location'],
+      component: function (deps) {
+        var location = deps.location;
+        var language = deps.languages[location];
+        return deps.utils.sprintf('This is my %s. We speak %s here.', location, language);
+      }
+    });
+
+    var printerComponentDefinition = new SimpleComponentDefinition({
+      name: targetComponentName,
+      scope: Scopes.singleton,
+      dependencies: ['message', 'utils', 'cities'],
+      component: function (deps) {
+        return deps.utils.sprintf('%s You may also want to travel to %s', deps.message, deps.cities[1]);
+      }
+    });
+
+    mockRegistry.expects('assemblyListFor').withArgs(targetComponentName).returns([
+      messageComponentDefinition,
+      utilsComponentDefinition,
+      citiesComponentDefinition,
+      printerComponentDefinition
+    ]).once();
+
+    mockRegistry.expects('assemblyListFor').withArgs('message').returns([
+      utilsComponentDefinition,
+      languagesComponentDefinition,
+      locationComponentDefinition,
+      messageComponentDefinition
+    ]).once();
+
+    mockRegistry.expects('assemblyListFor').withArgs('location').returns([
+      citiesComponentDefinition,
+      locationComponentDefinition
+    ]).once();
+
+    mockRegistry.expects('assemblyListFor').withArgs('utils').returns([
+      utilsComponentDefinition
+    ]).exactly(2);
+
+    mockRegistry.expects('assemblyListFor').withArgs('cities').returns([
+      citiesComponentDefinition
+    ]).exactly(2);
+
+    mockRegistry.expects('assemblyListFor').withArgs('languages').returns([
+      languagesComponentDefinition
+    ]).once();
+
+    Assembler.for(new AssemblyContext(targetComponentName, {
+      registry: registryApi
+    })).assemble().done(function (value) {
+      expect(value).to.be('This is my Mumbai. We speak Marathi here. You may also want to travel to Bangalore');
+      done();
+    });
+
   });
 
 });
