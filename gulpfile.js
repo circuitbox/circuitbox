75783634b4a213bfb3defdb389a86a8b11a34707
oddjobsman <oddjobsman@ranganathkini.com>
2014-01-17 19:59:04 +0530

    added support for coveralls

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,38 @@ var gulp = require('gulp');
 var tasks = require('gulp-load-tasks')({scope: ['devDependencies']});
 var stylish = require('jshint-stylish');
 
-function loadJsHintConfig() {
-  return JSON.parse(String(fs.readFileSync('./.jshintrc', 'utf8')));
-}
+var plumber = tasks.plumber;
 
 gulp.task('lint', function (done) {
   var jshint = tasks.jshint,
-      config = loadJsHintConfig();
+      config = JSON.parse(String(fs.readFileSync('./.jshintrc', 'utf8')));
 
   gulp.src(['./gulpfile.js', './lib/**/*.js', './test/**/*.js', './examples/**/*.js'])
+    .pipe(plumber())
+    .pipe(jshint(config))
+    .pipe(jshint.reporter(stylish))
     .on('end', function () {
       done();
-    })
-    .pipe(tasks.plumber())
-    .pipe(jshint(config))
-    .pipe(jshint.reporter(stylish));
+    });
 });
 
-gulp.task('test', ['lint'], function (done) {
-  gulp.src(['./test/**/*.js', '!./test/fixtures/*'])
+gulp.task('instrument', ['lint'], function (done) {
+  gulp.src(['./lib/**/*.js'])
+    .pipe(plumber())
+    .pipe(tasks.istanbul())
     .on('end', function () {
       done();
-    })
-    .pipe(tasks.plumber())
-    .pipe(tasks.mocha({reporter: 'spec'}));
+    });
+});
+
+gulp.task('test', ['instrument'], function (done) {
+  gulp.src(['./test/**/*.js', '!./test/fixtures/*'])
+      .pipe(plumber())
+      .pipe(tasks.mocha({reporter: 'spec'}))
+      .pipe(tasks.istanbul.writeReports())
+      .on('end', function () {
+        done();
+      });
 });
 
 gulp.task('sloc', function (done) {
