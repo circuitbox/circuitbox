feff59bda89721b0de3666ba85f184b3ce27de21
oddjobsman <oddjobsman@ranganathkini.com>
2014-02-11 17:58:32 +0530

    added a binding API to load bindings with circuitbox using the extender module

--- test/circuitbox.test.js
+++ test/circuitbox.test.js
@@ -7,6 +7,7 @@
 'use strict';
 
 var expect = require('expect.js');
+var sinon = require('sinon');
 
 var circuitbox = require('../lib');
 
@@ -61,6 +62,45 @@ describe('circuitbox', function () {
       expect(cbox).to.be.a(require('../lib/kernel'));
       done();
     });
+  });
+
+  it('should load a specified binding function passing it a binder and create a new circuitbox', function (done) {
+    /*jshint expr: true*/
+
+    var bindingSpy = sinon.spy();
+    var binder = require('../lib/binder');
+
+    circuitbox.withBindings(bindingSpy).create().done(function (cbox) {
+      expect(cbox).to.be.a(require('../lib/kernel'));
+
+      expect(bindingSpy.withArgs(binder).calledOnce).to.be(true);
+
+      done();
+    });
+
+  });
+
+  it('should load a specified binding module passing it a binder and create a new circuitbox', function (done) {
+    /*jshint expr: true*/
+
+    circuitbox.withBindings('./test/fixtures/aBinding').create().done(function (cbox) {
+      expect(cbox).to.be.a(require('../lib/kernel'));
+
+      done();
+    });
+
+  });
+
+  it('should prefix binding module id with "circuitbox-", then load it by passing it a binder and create a new circuitbox', function (done) {
+    /*jshint expr: true*/
+
+    circuitbox.withBindings('express').create().done(function () {
+      expect().fail();
+    }, function (err) {
+      expect(err.message).to.be('Cannot find module \'circuitbox-express\'');
+      done();
+    });
+
   });
 
 });
