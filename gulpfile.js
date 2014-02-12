3ffd358137883365139f68c81abaf968ea56a54f
oddjobsman <oddjobsman@ranganathkini.com>
2014-02-12 22:59:28 +0530

    refactored the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,48 @@
 'use strict';
 
-var fs = require('fs');
-var gulp = require('gulp');
-var tasks = require('gulp-load-plugins')({scope: ['devDependencies']});
-var stylish = require('jshint-stylish');
-
-var plumber = tasks.plumber;
+var gulp = require('gulp'),
+    tasks = require('gulp-load-plugins')({scope: ['devDependencies']}),
+    plumber = tasks.plumber;
 
 gulp.task('lint', function (done) {
-  var jshint = tasks.jshint,
-      config = JSON.parse(String(fs.readFileSync('./.jshintrc', 'utf8')));
+  var fs = require('fs'),
+      jshint = tasks.jshint,
+      config = JSON.parse(String(fs.readFileSync('./.jshintrc', 'utf8'))),
+      stylish = require('jshint-stylish');
 
   gulp.src(['./gulpfile.js', './lib/**/*.js', './test/**/*.js', './examples/**/*.js'])
     .pipe(plumber())
     .pipe(jshint(config))
     .pipe(jshint.reporter(stylish))
-    .on('end', function () {
-      done();
-    });
+    .on('end', done);
 });
 
 gulp.task('test', ['lint'], function (done) {
+  var mocha = tasks.mocha;
+
   gulp.src(['./test/**/*.js', '!./test/fixtures/*'])
-    .pipe(plumber())
-    .pipe(tasks.mocha({reporter: 'spec'}))
-    .on('end', function () {
-      done();
-    });
+//    .pipe(plumber())
+    .pipe(mocha({reporter: 'spec'}))
+    .on('end', done);
 });
 
 gulp.task('sloc', function (done) {
+  var sloc = tasks.sloc;
+
   gulp.src('./lib/**/*.js')
-    .pipe(tasks.plumber())
-    .pipe(tasks.sloc())
-    .on('end', function () {
-      done();
-    });
+    .pipe(plumber())
+    .pipe(sloc())
+    .on('end', done);
+
 });
 
 gulp.task('default', ['test', 'sloc'], function () {
+
   gulp.watch(['./gulpfile.js', './lib/**/*.js', './test/**/*.js', './examples/**/*.js'], function (event) {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     gulp.run('lint', 'test', 'sloc');
   });
+
 });
 
 gulp.task('ci', ['test', 'sloc'], function () {});
