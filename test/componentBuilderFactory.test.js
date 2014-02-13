b8464e88362333d54fc80f7c82296582f50d59a5
oddjobsman <oddjobsman@ranganathkini.com>
2014-02-13 18:42:08 +0530

    implemented ComponentBuilderFactory

--- test/componentBuilderFactory.test.js
+++ test/componentBuilderFactory.test.js
@@ -12,9 +12,36 @@ var expect = require('chai').expect,
 
 describe('ComponentBuilderFactory', function () {
 
-  it('should be able to register a builder', function () {
-    ComponentBuilderFactory.registerBulder('use', SimpleComponentBuilder);
-
-    var b = ComponentBuilderFactory.use('foo');
+  afterEach(function () {
+    ComponentBuilderFactory._reset();
   });
+
+  it('should be able to register a builder and create a ComponentDefinitionBuilder', function () {
+    var l = [],
+        b;
+
+    ComponentBuilderFactory.registerBuilder('use', SimpleComponentBuilder);
+
+    b = new ComponentBuilderFactory(l, 'myComponent').use('foo');
+
+    expect(b).to.be.instanceof(SimpleComponentBuilder);
+    expect(b.name).to.be.equal('myComponent');
+    expect(b.value).to.be.equal('foo');
+
+    expect(l.length).to.be.equal(1);
+    expect(l[0]).to.be.equal(b);
+
+  });
+
+  it('should throw error if a ComponentBuilder has already been registered with the specified method', function () {
+
+    ComponentBuilderFactory.registerBuilder('use', SimpleComponentBuilder);
+
+    expect(function () {
+      ComponentBuilderFactory.registerBuilder('use', SimpleComponentBuilder);
+    }).to.throw('builder already registered for method use');
+
+  });
+
+
 });
