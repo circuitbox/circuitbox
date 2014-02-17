12d98d4cbf3e2875c32f27760577566f941836ff
oddjobsman <oddjobsman@ranganathkini.com>
2014-02-17 08:50:51 +0530

    comments to ComponentCreatorFactory and added validation

--- test/componentCreatorFactory.test.js
+++ test/componentCreatorFactory.test.js
@@ -32,4 +32,31 @@ describe('ComponentCreatorFactory', function () {
     expect(cc.dependencies).to.be.empty;
   });
 
+  it('should register the specified ComponentCreator for the specified Component and dependencies and returns it when requested', function () {
+    var n = 'myComponent',
+        base = 'This is a component',
+        deps = ['foo', {name: 'bar'}],
+        c = new SimpleComponent(n, base),
+        cc;
+
+    ComponentCreatorFactory.registerCreator(SimpleComponent, SimpleComponentCreator);
+
+    cc = new ComponentCreatorFactory().creatorFor(c, deps);
+
+    expect(cc).to.be.instanceof(SimpleComponentCreator);
+    expect(cc.dependencies).to.be.eql(deps);
+  });
+
+  it('should throw error when there is no ComponentCreator registered for the specified registered Component type', function () {
+    var n = 'myComponent',
+        base = 'This is a component',
+        c = new SimpleComponent(n, base);
+
+    expect(function () {
+      /*jshint nonew: true*/
+
+      new ComponentCreatorFactory().creatorFor(c);
+    }).to.throw('no registered ComponentCreator for component type SimpleComponent');
+  });
+
 });
