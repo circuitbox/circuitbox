3a9025403722cdfcf6110fd1b14ce795447fa1a2
oddjobsman <oddjobsman@ranganathkini.com>
2014-02-25 07:52:35 +0530

    implemented Binder

---
+++ test/binder.test.js
@@ -0,0 +1,29 @@
+/*!
+ * circuitbox
+ * Copyright(c) 2014 Ranganath Kini <oddjobsman@ranganathkini.com>
+ * MIT Licensed
+ */
+
+'use strict';
+
+var expect = require('chai').expect,
+    ComponentDefinitionBuilderFactory = require('../lib/componentDefinitionBuilderFactory'),
+    ComponentCreatorFactory = require('../lib/componentCreatorFactory'),
+    ScopeHandlerFactory = require('../lib/scopeHandlerFactory'),
+    Binder = require('../lib/binder');
+
+describe('Binder', function () {
+
+  it('should expose the #registerBuilder method of ComponentDefinitionBuilderFactory to the bindings', function () {
+    expect(Binder.registerDefinitionBuilder).to.be.equal(ComponentDefinitionBuilderFactory.registerBuilder);
+  });
+
+  it('should expose the #registerCreator method of ComponentCreatorFactory to the bindings', function () {
+    expect(Binder.registerComponentCreator).to.be.equal(ComponentCreatorFactory.registerCreator);
+  });
+
+  it('should expose the #registerScopeHandler method of ScopeHandlerFactory to the bindings', function () {
+    expect(Binder.registerScopeHandler).to.be.equal(ScopeHandlerFactory.registerScopeHandler);
+  });
+
+});
