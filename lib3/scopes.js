13baf3bfd44ed4a325478e64613e0b278b187d55
oddjobsman <oddjobsman@ranganathkini.com>
2014-02-25 23:34:12 +0530

    deleted v1.x code artifacts and tests

--- lib3/scopes.js
+++
@@ -1,9 +0,0 @@
-/*!
- * circuitbox
- * Copyright(c) 2013 Ranganath Kini <oddjobsman@ranganathkini.com>
- * MIT Licensed
- */
-
-'use strict';
-
-module.exports = require('./utils').enumFor('prototype', 'singleton');
--- test3/fixtures/aBinding.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var expect = require('expect.js');
-
-module.exports = function aBinding(binder) {
-  expect(binder).to.be(require('.././binder'));
-};
