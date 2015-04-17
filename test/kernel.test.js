29a85e2839f589383bcb4a0143e5dd985f7d9d23
Ranganath Kini <codematix@codematix.me>
2015-04-17 23:00:59 +0530

    fix broken syntax in test

--- test/kernel.test.js
+++ test/kernel.test.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2014-2015 Ranganath Kini <codematix@codematix.me>
  * Copyright (c) 2015 intuitivcloud Systems <engineering@intuitivcloud.com>
  * MIT Licensed
- *//
+ */
 
 'use strict';
 
