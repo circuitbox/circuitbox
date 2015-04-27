0e83ee19a918a2de776cf8c92f997fae39cc7471
codematix <codematix@codematix.me>
2015-04-27 08:40:01 +0530

    modify defaultBindings to register type, regexp and name selectors with Binder

--- test/defaultBindings.test.js
+++ test/defaultBindings.test.js
@@ -10,18 +10,25 @@
 var r = require,
     sinon = r('sinon'),
     DefaultBindings = r('../lib/defaultBindings'),
+    SelectorFactory = r('../lib/selectorFactory'),
     binderApi = {
       registerDefinitionBuilder: function () {},
       registerComponentCreator: function () {},
-      registerScopeHandler: function () {}
+      registerScopeHandler: function () {},
+      registerSelector: function () {}
     };
 
 describe('DefaultBindings', function () {
   /*jshint expr: true*/
 
+  afterEach(function () {
+    SelectorFactory._reset();
+  });
+
   it('should register the default ComponentDefinitionBuilders, ComponentCreators and ScopeHandlers', function () {
     /*jshint newcap: false*/
-    var mB = sinon.mock(binderApi);
+    var mB = sinon.mock(binderApi),
+        defaultSelectors = r('../lib/defaultSelectors');
 
     mB.expects('registerDefinitionBuilder').withArgs('use', r('../lib/simpleComponentDefinitionBuilder')).once();
     mB.expects('registerDefinitionBuilder').withArgs('requires', r('../lib/moduleComponentDefinitionBuilder')).once();
@@ -31,6 +38,10 @@ describe('DefaultBindings', function () {
 
     mB.expects('registerScopeHandler').withArgs('singleton', r('../lib/singletonScopeHandler')).once();
     mB.expects('registerScopeHandler').withArgs('prototype', r('../lib/prototypeScopeHandler')).once();
+
+    mB.expects('registerSelector').withArgs('type', defaultSelectors.type).once();
+    mB.expects('registerSelector').withArgs('regexp', defaultSelectors.regexp).once();
+    mB.expects('registerSelector').withArgs('name', defaultSelectors.name).once();
 
     DefaultBindings(binderApi);
 
